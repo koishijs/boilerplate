0f745b410171b0f2c763ca40859bdd6dce42a6d3
Shigma <shigma10826@gmail.com>
2022-06-29 19:50:29 +0800

    support optional dependencies

--- .github/workflows/sync.sh
+++ .github/workflows/sync.sh
@@ -2,15 +2,20 @@ mkdir -p $RUNNER_TEMP/dist
 cp -a * $RUNNER_TEMP/dist
 cp -a .github $RUNNER_TEMP/dist
 cp .* $RUNNER_TEMP/dist
-
 mv $RUNNER_TEMP/dist dist
 
-cd dist/.github/workflows
+# modify package.json
+cd dist
 
+sed -i package.json \
+-e '/"version": /c\
+  "version": "0.0.0",'
+-e '/"optionalDependencies": {/,/},/d'
+
+# modify workflows
+cd .github/workflows
 rm sync.sh
 rm sync.yml
 
 sed -i tag.yml \
--e 's/develop/master/g' \
--e "/ubuntu-latest/i\\
-    if: \${{ github.repository != 'koishijs/boilerplate' }}"
+-e 's/develop/master/g'
--- .github/workflows/tag.sh
+++ .github/workflows/tag.sh
@@ -1,4 +1,9 @@
 TAG=v$(cat package.json | grep '"version":' | cut -d '"' -f 4)
+
+if [ $TAG == '0.0.0' ]; then
+  exit 0
+fi
+
 REG=^$(echo $TAG | sed 's/\./\\./g')$
 
 if [ -z "$(git tag -l | grep $REG)" ]; then
