4e7941fe67f012f10f5f0e4796fb1d9b2ff4e5d6
H4M5TER <zhuxx2003@gmail.com>
2022-10-22 01:18:41 +0800

    migrate docker

--- .github/workflows/sync.sh
+++ .github/workflows/sync.sh
@@ -4,16 +4,18 @@ cp -a .github $RUNNER_TEMP/dist
 cp .* $RUNNER_TEMP/dist
 mv $RUNNER_TEMP/dist dist
 
-# modify package.json
 cd dist
 
+# modify package.json
 cat package.json | jq '.version="0.0.0" | del(.optionalDependencies)' > package.json.tmp
 mv -f package.json.tmp package.json
 
 # modify workflows
+rm -r docker/
 cd .github/workflows
 rm sync.sh
 rm sync.yml
+rm docker.yml
 
 sed -i tag.yml \
 -e 's/develop/master/g'
