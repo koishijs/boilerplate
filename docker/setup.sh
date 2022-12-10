3eef2d8a54cea9d833fa044f46da89eaf3eeec2a
H4M5TER <zhuxx2003@gmail.com>
2022-12-11 01:42:58 +0800

    fix docker build

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 apk add unzip curl jq
-# trim "linux/"
+# "linux/amd64" -> "amd64"
 ARCH=${TARGETPLATFORM:6}
-NAME=$(cat package.json | jq -r '.name' | sed -E 's/.+\///')
-TAG=$(curl "https://api.github.com/repos/$REPO/releases/latest" | jq -r '.tag_name')
-LINK="https://github.com/$REPO/releases/download/$TAG/$NAME-$TAG-linux-$ARCH-node16.zip"
+# LINK="https://github.com/$REPO/releases/download/$TAG/$NAME-$TAG-linux-"
+LINK=$LINK$ARCH-node16.zip
 echo $LINK && curl -L -o bundle.zip $LINK
 unzip bundle.zip -d /koishi
 sed -i 's/host: .*/host: 0.0.0.0/g' /koishi/koishi.yml
