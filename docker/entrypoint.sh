6bfe33cc75aed814f5fadc9f943e81d5310d5667
Anillc <i@anillc.cn>
2023-12-13 01:23:34 +0800

    fix: append host after maxPort (#31)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 chown -R root:root /koishi
 if [ ! -e "/koishi/package.json" ]; then
   unzip -d /koishi /boilerplate.zip
-  sed -i '1i host: 0.0.0.0' /koishi/koishi.yml
+  sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
 fi
 
 exec "$@"
